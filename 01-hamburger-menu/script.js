918de601aaa9217302884863a97bb79918292b68
soeunSim <629943@naver.com>
2025-05-22 22:01:29 +0900

    feat: 햄버거 메뉴 기능에 접근성 속성 적용

--- 01-hamburger-menu/script.js
+++ 01-hamburger-menu/script.js
@@ -4,8 +4,12 @@ const selectCloseBtn = document.getElementById("closeBtn");
 
 selectButton.addEventListener("click", function() {
     selectNav.classList.add("on");
+    selectButton.setAttribute("aria-expanded", "true");
+    selectNav.setAttribute("aria-hidden", "false");
 });
 
 selectCloseBtn.addEventListener("click", function() {
   selectNav.classList.remove("on");
+  selectButton.setAttribute("aria-expanded", "false");
+  selectNav.setAttribute("aria-hidden", "true");
 });
