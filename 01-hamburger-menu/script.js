71f9c878ba3fc648af220eb36ed43f192b540a72
soeunSim <629943@naver.com>
2025-05-23 01:43:44 +0900

    feat: 햄버거 메뉴 활성화 시 포커스트랩 구현

--- 01-hamburger-menu/script.js
+++ 01-hamburger-menu/script.js
@@ -1,29 +1,68 @@
 const selectButton = document.getElementById("menuToggle");
 const selectNav = document.getElementById("sideMenu");
 const selectCloseBtn = document.getElementById("closeBtn");
+const selectMenuOverlay = document.getElementById("overlay");
+
+const focusableElements = ` a[href], button, textarea, input, select, [tabindex]:not([tabindex="-1"])`;
+const focusables = selectNav.querySelectorAll(focusableElements);
+const firstFocus = focusables[0];
+const lastFocus = focusables[focusables.length - 1];
 
 selectButton.addEventListener("click", function() {
-    selectNav.classList.add("on");
-    selectButton.setAttribute("aria-expanded", "true");
-    selectNav.setAttribute("aria-hidden", "false");
+  selectNav.classList.add("on");
+  selectMenuOverlay.classList.remove("hidden");
+
+  selectButton.setAttribute("aria-expanded", "true");
+  selectNav.setAttribute("aria-hidden", "false");
+  selectMenuOverlay.setAttribute("aria-hidden", "false");
+  document.body.style.overflow = 'hidden'; 
+
+  requestAnimationFrame(() => {
+    firstFocus.focus();
+  });    
 });
 
 selectCloseBtn.addEventListener("click", function() {
-    if( selectNav.classList.contains("on")) {
-      closeMenu();
-    }
+    closeMenu();
 });
 
 window.addEventListener('keyup', (event) => {
   if(event.key === "Escape") {
-    if( selectNav.classList.contains("on")) {
-      closeMenu();
-    }
+    closeMenu();
   }
 })
 
+selectMenuOverlay.addEventListener("click", function() {
+  closeMenu();
+});
+
 function closeMenu() {
+  if (!selectNav.classList.contains("on")) return;
+
   selectNav.classList.remove("on");
+  selectMenuOverlay.classList.add("hidden");
+  selectButton.focus();
+
   selectButton.setAttribute("aria-expanded", "false");
-  selectNav.setAttribute("aria-hidden", "true"); 
+  selectNav.setAttribute("aria-hidden", "true");
+  selectMenuOverlay.setAttribute("aria-hidden", "true"); 
+  document.body.style.overflow = '';
 }
+
+selectNav.addEventListener("keydown", (event) => {
+  const checkKeyEventTab = event.key === "Tab";
+  const checkKeyWidthShift = event.shiftKey;
+
+  if(checkKeyEventTab && checkKeyWidthShift) {
+    if(document.activeElement === firstFocus) {
+      event.preventDefault();
+      lastFocus.focus();
+    }
+  }
+  if(checkKeyEventTab) {
+    if(document.activeElement === lastFocus) {
+      event.preventDefault();
+      firstFocus.focus();
+    }
+  }  
+});
